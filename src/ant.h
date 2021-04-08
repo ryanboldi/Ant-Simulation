23fa13dc09d741cec4cb4bfce1de931ed856371a
Ryan Boldi <ryan.boldi123@gmail.com>
2021-04-08 18:52:04 +0400

    Update ant.h

--- src/ant.h
+++ src/ant.h
@@ -3,20 +3,29 @@
 
 #include <SDL.h>
 
+#define ANT_WALK_FRAMES 4
+
 class Ant
 {
 private:
     float x, y;
     float angle;
     unsigned short int scale = 1;
+    static SDL_Rect gSprintClips[ANT_WALK_FRAMES];
 
 public:
     // constructor
-    Ant(float x, float y, unsigned short int scale = 1, float angle = 0) : x(x), y(y), scale(scale), angle(angle){};
+    Ant(float x, float y, unsigned short int scale = 1, float angle = 0) : x(x),
+                                                                           y(y),
+                                                                           scale(scale),
+                                                                           angle(angle){};
     ~Ant();
 
+    static SDL_Rect loadSprites();
+
     //getters
-    float get_x();
+    float
+    get_x();
     float get_y();
     float get_angle();
 
